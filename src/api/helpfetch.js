3d60af7ae39822869b8eb103c0f8179fe89e6a8c
Shaiel Becerra <shaielbecerra@gmail.com>
2025-02-02 11:42:10 -0400

    mejorar la función customFetch para manejar encabezados y errores de manera más eficiente

--- src/api/helpfetch.js
+++ src/api/helpfetch.js
@@ -1,43 +1,53 @@
 export const helpFetch = () => {
-  const URL = "https://json-alcaldia-1.onrender.com/";
-
-  const customFetch = (endpoint, options = {}) => {
-      options.method = options.method || "GET";
-      options.headers = {
-          "content-type": "application/json"
-      };
-
-      if (options.body) {
-          options.body = JSON.stringify(options.body);
+    const URL = "https://json-alcaldia-1.onrender.com/"
+  
+    const customFetch = (endpoint, options = {}) => {
+      options.method = options.method || "GET"
+  
+      if (!(options.body instanceof FormData)) {
+        options.headers = {
+          "Content-Type": "application/json",
+          ...options.headers,
+        }
+        if (options.body) {
+          options.body = JSON.stringify(options.body)
+        }
       }
-
-      return fetch(`${URL}${endpoint}`, options).then(response => {
-          return response.ok ? response.json() : Promise.reject({
+  
+      return fetch(`${URL}${endpoint}`, options)
+        .then((response) => {
+          if (!response.ok) {
+            return Promise.reject({
               error: true,
               status: response.status,
-              statusText: response.statusText
-          });
-      }).catch(error => error);
-  };
-
-  const get = (endpoint) => customFetch(endpoint);
-
-  const post = (endpoint, options) => {
-      options.method = "POST";
-      return customFetch(endpoint, options);
-  };
-
-  const put = (endpoint, options, id) => {
-      options.method = "PUT";
-      return customFetch(`${endpoint}/${id}`, options);
-  };
-
-  const delet = (endpoint, id) => {
+              statusText: response.statusText,
+            })
+          }
+          return response.json()
+        })
+        .catch((error) => error)
+    }
+  
+    const get = (endpoint) => customFetch(endpoint)
+  
+    const post = (endpoint, options) => {
+      options.method = "POST"
+      return customFetch(endpoint, options)
+    }
+  
+    const put = (endpoint, options) => {
+      options.method = "PUT"
+      return customFetch(endpoint, options)
+    }
+  
+    const del = (endpoint, id) => {
       const options = {
-          method: "DELETE"
-      };
-      return customFetch(`${endpoint}/${id}`, options);
-  };
-
-  return { get, post, put, delet };
-};
+        method: "DELETE",
+      }
+      return customFetch(`${endpoint}/${id}`, options)
+    }
+  
+    return { get, post, put, del }
+  }
+  
+  
